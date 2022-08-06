0cf4e2752b4fa0b143d7ddacc5b0d6162e7a217d
Athena Cheng <athenacheng@Hane-MacBook-Pro.local>
2022-08-06 15:09:26 +0800

    init commit

---
+++ part_5/app.js
@@ -0,0 +1,81 @@
+function Navbar() {
+	return (
+		<div>
+			<nav className="navbar">
+				<h1>Website Title / Logo</h1>
+				<button type="button" className="ham">
+					&#9776;
+				</button>
+				<ul className="nav-list">
+					<li className="nav-list-item">Item 1</li>
+					<li className="nav-list-item">Item 2</li>
+					<li className="nav-list-item">Item 3</li>
+					<li className="nav-list-item">Item 4</li>
+				</ul>
+
+				<button type="button" className="close-btn">
+					X
+				</button>
+				<ul className="h-menu">
+					<li className="h-menu-item">Item 1</li>
+					<li className="h-menu-item">Item 2</li>
+					<li className="h-menu-item">Item 3</li>
+					<li className="h-menu-item">Item 4</li>
+				</ul>
+			</nav>
+		</div>
+	);
+}
+
+function Welcome() {
+	return (
+		<div>
+			{/*<!---------- welcome message ---------->*/}
+			<div className="welcome-message">
+				<p>Welcome Message</p>
+			</div>
+		</div>
+	);
+}
+
+function Content() {
+	return (
+		<div>
+			{/*<!---------- section title ---------->*/}
+			<div className="section-title">
+				<p>Section Title</p>
+			</div>
+
+			{/*<!---------- content ---------->*/}
+			<div className="content">
+				<div className="content-box">Content Box 1</div>
+				<div className="content-box">Content Box 2</div>
+				<div className="content-box">Content Box 3</div>
+				<div className="content-box">Content Box 4</div>
+				<button type="button" className="action-btn">
+					Call to Action
+				</button>
+			</div>
+
+			<div className="content hide-content">
+				<div className="content-box">Content Box 5</div>
+				<div className="content-box">Content Box 6</div>
+				<div className="content-box">Content Box 7</div>
+				<div className="content-box">Content Box 8</div>
+			</div>
+		</div>
+	);
+}
+
+function App() {
+	return (
+		<div>
+			<Navbar />
+			<Welcome />
+			<Content />
+		</div>
+	);
+}
+
+//usually render tpo level elements into the DOM
+ReactDOM.render(<App />, document.getElementById("root"));
